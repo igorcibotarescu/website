60dc8216ff496ee8a6773695a98a0d9a110fff30
Igor Cibotarescu <cibotarescu00@mail.ru>
2022-02-23 15:04:35 +0200

    Update confirm_email.php

--- utilities/confirm_email.php
+++ utilities/confirm_email.php
@@ -20,8 +20,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'igormanolescu@gmail.com';                     //SMTP username
-    $mail->Password   = 'mamika4xd';                               //SMTP password
+    $mail->Username   = 'yourusername';                     //SMTP username
+    $mail->Password   = 'yourpassword';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
